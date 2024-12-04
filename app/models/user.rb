fa63f3a7538aa4829b113009f91fdd77344c7238
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-04 14:52:53 +0900

    Active Storageの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,13 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :image, presence: true
+  validates :profile_image, presence: true
   validates :age_group_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :gender_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+
+
+  has_one_attached :image
 end
