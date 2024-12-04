38d02a79f923880c445e5954224a76a845ec1516
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-04 14:28:42 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  
+  validates :nickname, presence: true
+  validates :image, presence: true
+  validates :age_group_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :gender_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
 end
