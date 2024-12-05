f76f798bd3adff6ac5dc79315ca4940597d5e1a1
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-06 03:44:28 +0900

    不要なマイグレーションファイルの削除

--- db/migrate/20241204062051_rename_column_in_users.rb
+++
@@ -1,6 +0,0 @@
-class RenameColumnInUsers < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :age_group, :age_group_id
-    rename_column :users, :gender, :gender_id
-  end
-end
