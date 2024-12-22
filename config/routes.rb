8106aef0412e4616f2f7eb6d92e69d28580b7e96
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-22 21:38:32 +0900

    コントローラーの作成とルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end  
   resources :users
+  resources :reviews
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
