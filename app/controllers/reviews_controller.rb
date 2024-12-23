24e5878f601847bdb46e456912b676c530885276
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-23 19:17:51 +0900

    詳細表示機能

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -18,6 +18,9 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def show
+    @review = current_user.reviews.find(params[:id])
+  end
   private
   def review_params
     params.require(:review).permit(:title, :rating, :comment, :image)
