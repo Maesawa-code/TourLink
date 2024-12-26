42d57e014100bca31930f041a6470a43b2f8ffea
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-26 19:38:08 +0900

    テストコードの実装

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :review do
-    
+    association :user
+    title { Faker::Lorem.sentence(word_count: 3) }
+    rating { Faker::Number.between(from: 1, to: 5) }
+    comment { Faker::Lorem.sentence(word_count: 10) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test_image.png'), 'image/png') }
   end
 end
