51473cd5f72ef0dccfc8a01e40000abe7e1376c4
Maesawa-code <hirokitechcamp@gmail.com>
2024-12-26 16:59:54 +0900

    バリデーションとエラーメッセージの設定

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,6 @@ class Review < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 100 }
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :comment, length: { maximum: 300 }
+  validates :comment, length: { maximum: 100 }
+  validates :image, presence: true
 end
